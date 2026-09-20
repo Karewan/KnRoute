c72578f54f4b645bcde4f4c21f75efd8037d6cf3
Florent VIALATTE <Karewan@users.noreply.github.com>
2026-09-20 08:14:41 +0200

    Added Connect route attribute

---
+++ src/Attributes/Connect.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Karewan\KnRoute\Attributes;
+
+use Attribute;
+
+#[Attribute(Attribute::IS_REPEATABLE | Attribute::TARGET_METHOD)]
+class Connect extends Route
+{
+	/**
+	 * CONNECT route
+	 * @param string $path
+	 * @return void
+	 */
+	public function __construct(string $path)
+	{
+		parent::__construct(['CONNECT'], $path);
+	}
+}
